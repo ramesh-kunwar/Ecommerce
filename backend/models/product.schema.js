8da5de4a8f8fc564607597def2e448f4d9a2650a
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-10-23 14:39:37 +0545

    product schema added

--- backend/models/product.schema.js
+++ backend/models/product.schema.js
@@ -4,12 +4,39 @@ const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Collection name is required"],
+      required: [true, "Product name is required"],
       trim: true,
-      maxLength: [
-        120,
-        "Collection name should not be more than 120 characters",
-      ],
+      maxLength: [120, "Product name should not be more than 120 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Poduct price is required"],
+      maxLength: [5, "Product price should not be more than 5 digits"],
+    },
+    description: {
+      type: String,
+      // use some form of editor - personal assignment
+    },
+    photos: [
+      {
+        secure_url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    collectionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Collection",
     },
   },
   { timestamps: true }
