d176a3cb4e5b085004e18f03872d24bbb4a33321
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-10-23 07:38:58 +0545

    folder cleanup

--- src/pages/CartPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Cart from "../features/Cart/Cart";
-
-const CartPage = () => {
-  return <Cart />;
-};
-
-export default CartPage;
--- src/pages/ProductDetailPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProductDetail from "../features/ProductList/Components/ProductDetail";
-
-const ProductDetailPage = () => {
-  return <ProductDetail />;
-};
-
-export default ProductDetailPage;
