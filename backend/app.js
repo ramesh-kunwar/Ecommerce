a1ee6fd8c05ff1fe05fe0ec9ed2cb4701c25b72d
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-10-23 16:06:36 +0545

    added morgan and used middleware

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // when we want to use form data
+app.use(cors())
+app.use(cookieParser());
+
+// morgan logger
+app.use(morgan('tiny'))
 
 export default app;
